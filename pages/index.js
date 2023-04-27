0a42a95c5a07d373882d3b9cfe5f8f0d079b7593
Meka Seymour <mekaseymour@gmail.com>
2023-04-27 19:55:33 -0400

    minor changes

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import cn from 'classnames'
 import Image from 'next/image';
 
 import birdGif from '../images/bird.gif';
@@ -21,20 +22,20 @@ export default function Home() {
       <div>
         <h1 className={styles.title}>Hi, I'm Meka</h1>
         <div className={styles.description}>
-          <p>I’m a software engineer with over five years of experience building at early to mid-stage startups (and occasionally elsewhere).</p>
+          <p>I’m a software engineer with over five years of experience building products at early to mid-stage startups (and occasionally elsewhere).</p>
           <p>Teaching and walking around Harlem all other times.</p>
           <p>If you’re working on a project (esp. in climate or health) and want to chat feel free to reach out.</p>
         </div>
-        <div>
-          <button className={styles.primary}>Drop me an email</button>
-          <button>☕️ Schedule some time</button>
+        <div className={styles.buttonContainer}>
+          <a href="mailto:mekaseymour@gmail.com" className={cn(styles.button, styles.primary)}>Drop me an email</a>
+          <a href="/" className={styles.button}>☕️ Schedule some time</a>
         </div>
       </div>
       <div className={styles.socialContainer}>
-        <a href="https://www.linkedin.com/in/mekaseymour/">
+        <a href="https://www.linkedin.com/in/mekaseymour/" target="_blank">
           <Image className={styles.socialLink} src={pandaGif}  height={50} width={50} alt="link to LinkedIn" />
         </a>
-        <a href="https://twitter.com/mekaseymour">
+        <a href="https://twitter.com/mekaseymour" target="_blank">
           <Image className={styles.socialLink} src={birdGif} height={50} width={50} alt="link to Twitter" />
         </a>
       </div>
